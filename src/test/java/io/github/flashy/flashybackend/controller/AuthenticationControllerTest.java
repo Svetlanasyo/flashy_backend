0595ccef22891d362b9c4461c1e7b77220010aad
Jarosław Wałach <jaroslaw.walach@gmail.com>
2019-02-12 11:20:02 +0100

    Add test for registration endpoint and update of pom.xml

--- src/test/java/io/github/flashy/flashybackend/controller/AuthenticationControllerTest.java
+++ src/test/java/io/github/flashy/flashybackend/controller/AuthenticationControllerTest.java
@@ -1,7 +1,8 @@
 package io.github.flashy.flashybackend.controller;
 
 import io.github.flashy.flashybackend.model.User;
-import io.github.flashy.flashybackend.repositories.UserRepository;
+import io.github.flashy.flashybackend.model.UserDto;
+import io.github.flashy.flashybackend.service.UserService;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -30,18 +33,32 @@ public class AuthenticationControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private UserRepository userRepository;
+    private UserService userService;
 
-    private List<User> usersdb;
+    private List<UserDto> usersdb;
 
 
     @Before
     public void setupTest() {
-        when(userRepository.save(any(User.class))).thenAnswer(invocation -> {
+        when(userService.registerNewUser(any(User.class))).thenAnswer(invocation -> {
             User user = invocation.getArgument(0);
-            usersdb.add(user);
+            UserDto dto = new UserDto();
 
-            return user;
+            dto.setId(usersdb.size());
+            dto.setNickName(user.getNickName());
+            dto.setPassword(user.getPassword());
+
+            usersdb.add(dto);
+
+            return dto;
+        });
+
+        when(userService.canLogin(anyString(), anyString())).thenAnswer(invocation -> {
+            String username = invocation.getArgument(0);
+            String password = invocation.getArgument(1);
+
+            return usersdb.stream()
+                    .anyMatch(dto -> dto.getNickName().equals(username) && dto.getPassword().equals(password));
         });
 
         usersdb = new ArrayList<>();
@@ -49,7 +66,6 @@ public class AuthenticationControllerTest {
 
     @Test
     public void testReturn201IfUserRegistered() throws Exception {
-        // When: I send that to rest endpoint /register
         JSONObject json = new JSONObject();
         json.put("nick_name", "user");
         json.put("password", "user1234");
@@ -64,6 +80,38 @@ public class AuthenticationControllerTest {
         actions.andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
 
-        verify(userRepository).save(any(User.class));
+        verify(userService).registerNewUser(any(User.class));
+    }
+
+    @Test
+    public void testReturnAcceptedIfUserLogin() throws Exception {
+        // given
+
+        UserDto user = new UserDto();
+        user.setId(0);
+        user.setNickName("user");
+        user.setPassword("password");
+
+        usersdb.add(user);
+
+        JSONObject json = new JSONObject();
+        json.put("nick_name", "user");
+        json.put("password", "password");
+
+        ResultActions actions = mockMvc.perform(
+                post("/login")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                        .content(json.toString())
+        );
+
+        // Expected:
+        JSONObject expecteJson = new JSONObject();
+        expecteJson.put("logged_in", "true");
+
+
+        actions.andExpect(status().isAccepted())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+
+        verify(userService).canLogin(anyString(), anyString());
     }
 }
